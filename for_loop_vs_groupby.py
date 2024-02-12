3301125906b44ea2011d21027efdcdb61dacaa60
Dmitry Chigarev <dmitry.chigarev@intel.com>
2024-02-12 06:18:02 -0700

    Initial commit
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

---
+++ for_loop_vs_groupby.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+NROWS = 10_000_000
+NUM_CODES = 1_000_000
+NREFERENCES = 10_000
+NCOLS = 10
+
+data = {
+    "Goods": np.tile(np.arange(NUM_CODES), NROWS // NUM_CODES),
+    "Price": np.random.randint(100, 150, size=NROWS),
+    **{f"data_col{i}": np.random.randint(0, 1_000_000, size=NROWS) for i in range(NCOLS - 2)}
+}
+
+reference_mean_prices = {
+    "Goods": np.random.choice(np.arange(NUM_CODES), size=NREFERENCES, replace=False),
+    "Price": np.random.randint(110, 130, size=NREFERENCES)
+}
+
+import modin.pandas as pd
+# import pandas as pd
+
+from modin.utils import execute
+import modin.config as cfg
+pd.DataFrame(np.arange(cfg.NPartitions.get() * cfg.MinPartitionSize.get())).to_numpy()
+
+df = pd.DataFrame(data)
+reference_mean_prices = pd.Series(reference_mean_prices["Price"], index=reference_mean_prices["Goods"]).sort_index()
+
+from timeit import default_timer as timer
+
+execute(df)
+execute(reference_mean_prices)
+
+
+def case1_for_loop(df, reference_prices):
+    """Beware, this function executes very long: ~80s on Pandas and ~900s on Modin."""
+    # Task: mark ‘Goods’ with 'ones', whose mean price across different
+    # branches is greater than the threshold from 'reference_prices'
+    # ‘df’ shape: (10_000_000, 10)
+    # Num unique goods: 1_000_000
+    # ‘len(reference_prices)’: 10_000
+    df["Flag"] = 0
+    iters = []
+    i = 0
+    for threshold_price, code in reference_prices.items():
+        t1 = timer()
+        mean_price = df[df["Goods"] == code]["Price"].mean()
+        flag_value = mean_price > threshold_price
+        if mean_price > threshold_price:
+            df.loc[df["Goods"] == code, "Flag"] = 1
+        iters.append(timer() - t1)
+        i += 1
+        if i > 10:
+            break
+    print("mean", np.mean(iters))
+    return df
+
+def case2_groupby_mean(df, reference_mean_prices):
+    # The same code, but using groupby.mean() + .apply()
+    actual_mean_prices = (
+        df[["Goods", "Price"]][
+            df["Goods"].isin(reference_mean_prices.index)
+        ]
+        .groupby("Goods")["Price"]
+        .mean()
+    )
+    mask = actual_mean_prices > reference_mean_prices
+    df["Flag"] = df["Goods"].apply(
+        lambda code, mask: int(mask.get(code, False)), args=(mask,)
+    )
+    return df
+
+print("data done...")
+
+t1 = timer()
+res = case2_groupby_mean(df, reference_mean_prices)
+execute(res)
+len(res)
+print("time:", timer() - t1)
---
+++ heavy_operations.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+from timeit import default_timer as timer
+
+import pandas as pd
+import modin.config as cfg
+# TODO: enable range-partitioning impl for groupby().rolling() by default in Modin
+# and remove this
+cfg.RangePartitioningGroupby.put(True)
+from modin.pandas.io import from_pandas, to_pandas, read_csv
+
+# initialize ray
+to_pandas(from_pandas(pd.DataFrame({"a": [1]})))
+
+def measure_apply():
+
+    def users_active_at_date(date):
+        return [np.random.randint(0, 1_000_000) for _ in range(10)]
+
+    apply_func = lambda row: str(users_active_at_date(row.date())[0]).zfill(2)
+
+    df = pd.DataFrame({"date_col": pd.to_datetime(np.random.randint(0, 1_000_000, size=1_000_000))})
+
+    print("Data generated")
+
+    t1 = timer()
+    res = df["date_col"].apply(apply_func)
+    pd_time = timer() - t1
+
+    t1 = timer()
+    res = to_pandas(from_pandas(df)["date_col"].apply(apply_func))
+    md_time = timer() - t1
+    print(f"apply: {pd_time=}; {md_time=}")
+
+
+def measure_merge():
+    # Enabling AsyncReading is recommended for big dataframes. It consumes more memory,
+    # but from_pandas() conversion works noticeably faster
+    cfg.AsyncReadMode.put(True)
+    df1_nrows = 80_000_000
+    df1_ncols = 10
+    df1_nkeys = 20_000_000
+
+    df2_nrows = 6_000_000
+    df2_ncols = 5
+    df2_nkeys = 6_000_000
+
+    df1_data = {
+        "key": np.random.randint(0, df1_nkeys, size=df1_nrows),
+        **{f"data{i}": np.random.randint(0, 1_000_000, size=df1_nrows) for i in range(df1_ncols - 1)}
+    }
+
+    df2_data = {
+        "key": np.random.randint(0, df2_nkeys, size=df2_nrows),
+        **{f"data{i}": np.random.randint(0, 1_000_000, size=df2_nrows) for i in range(df2_ncols - 1)}
+    }
+
+    df1 = pd.DataFrame(df1_data)
+    df2 = pd.DataFrame(df2_data)
+
+    t1 = timer()
+    df1.merge(df2, on="key")
+    pd_time = timer() - t1
+
+
+    t1 = timer()
+    df1 = from_pandas(df1)
+    df2 = from_pandas(df2)
+    to_pandas(df1.merge(df2, on="key"))
+    md_time = timer() - t1
+    print(f"merge: {pd_time=}; {md_time=}")
+    cfg.AsyncReadMode.put(False)
+
+
+def measure_groupby_rolling():
+    NROWS = 10_000_000
+    NUM_GROUPS = 250_000
+    NCOLS = 10
+
+    data = {
+        "key": np.tile(np.arange(NUM_GROUPS), NROWS // NUM_GROUPS),
+        **{f"data_col{i}": np.random.randint(0, 1_000_000, size=NROWS) for i in range(NCOLS - 1)}
+    }
+
+    df = pd.DataFrame(data)
+    print("data done...")
+
+    t1 = timer()
+    df.groupby("key").rolling(10).mean()
+    pd_time = timer() - t1
+    print("pandas done...")
+
+    t1 = timer()
+    to_pandas(from_pandas(df).groupby("key").rolling(10).mean())
+    md_time = timer() - t1
+
+    print(f"groupby: {pd_time=}; {md_time=}")
+
+
+def measure_read_csv():
+    NROWS = 40_000_000
+    NCOLS = 10
+
+    data = {
+        f"data_col{i}": np.random.randint(0, 1_000_000, size=NROWS) for i in range(NCOLS)
+    }
+
+    import tempfile
+    with tempfile.NamedTemporaryFile() as file:
+        df = pd.DataFrame(data).to_csv(file.name)
+        print("data done...")
+
+        t1 = timer()
+        pd.read_csv(file.name)
+        pd_time = timer() - t1
+        print("pandas done...")
+
+        t1 = timer()
+        to_pandas(read_csv(file.name))
+        md_time = timer() - t1
+
+        print(f"groupby: {pd_time=}; {md_time=}")
+
+
+measure_read_csv()
+measure_apply()
+measure_merge()
+measure_groupby_rolling()
