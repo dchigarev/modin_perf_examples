7996f25468e9d82167f9f65521caccd4eba26d2c
Dmitry Chigarev <dmitry.chigarev@intel.com>
2024-03-04 05:29:48 -0700

    correct examples
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- for_loop_vs_groupby.py
+++ for_loop_vs_groupby.py
@@ -5,6 +5,8 @@ NUM_CODES = 1_000_000
 NREFERENCES = 10_000
 NCOLS = 10
 
+print("generating data...")
+
 data = {
     "Goods": np.tile(np.arange(NUM_CODES), NROWS // NUM_CODES),
     "Price": np.random.randint(100, 150, size=NROWS),
@@ -19,10 +21,8 @@ reference_mean_prices = {
     "Price": np.random.randint(110, 130, size=NREFERENCES),
 }
 
-# select here which library you want to use for the measurements
 import modin.pandas as pd
-
-# import pandas as pd
+import pandas
 
 from modin.utils import execute
 import modin.config as cfg
@@ -40,14 +40,17 @@ from timeit import default_timer as timer
 execute(df)
 execute(reference_mean_prices)
 
+print("data generated")
 
-def case1_for_loop(df, reference_prices):
-    """Beware, this function executes very long: ~80s on Pandas and ~900s on Modin."""
+
+def case1_for_loop(df, reference_prices, run_whole_loop=False):
+    """Beware, this function executes very with 'run_whole_loop=True' long: ~80s on Pandas and ~900s on Modin."""
     # Task: mark ‘Goods’ with 'ones', whose mean price across different
     # branches is greater than the threshold from 'reference_prices'
     # ‘df’ shape: (10_000_000, 10)
     # Num unique goods: 1_000_000
     # ‘len(reference_prices)’: 10_000
+    t0 = timer()
     df["Flag"] = 0
     iters = []
     i = 0
@@ -58,10 +61,16 @@ def case1_for_loop(df, reference_prices):
             df.loc[df["Goods"] == code, "Flag"] = 1
         iters.append(timer() - t1)
         i += 1
-        if i > 10:
+        if not run_whole_loop and i > 10:
             break
-    print("mean", np.mean(iters))
-    return df
+    execute(df)
+    if run_whole_loop:
+        total_time = timer() - t0
+        mean_iter_time = np.mean(iters)
+    else:
+        total_time = np.mean(iters) * len(reference_prices)
+        mean_iter_time = np.mean(iters)
+    return round(total_time, 2), round(mean_iter_time, 4)
 
 
 def case2_groupby_mean(df, reference_mean_prices):
@@ -77,11 +86,25 @@ def case2_groupby_mean(df, reference_mean_prices):
     )
     return df
 
+def measure_cases(df, reference_mean_prices):
+    t1 = timer()
+    total_time, mean_iter_time = case1_for_loop(df, reference_mean_prices)
 
-print("data done...")
+    t1 = timer()
+    res = case2_groupby_mean(df, reference_mean_prices)
+    # trigger execution for Modin for fair measurement
+    execute(res)
+    second_case_time = round(timer() - t1, 2)
 
-t1 = timer()
-res = case2_groupby_mean(df, reference_mean_prices)
-# trigger execution for Modin for fair measurement
-execute(res)
-print("time:", timer() - t1)
+    return (total_time, mean_iter_time), second_case_time
+
+# modin
+print("running test cases for modin...")
+(md_first_t, md_first_m), md_second = measure_cases(df, reference_mean_prices)
+
+# pandas
+print("running test cases for pandas...")
+(pd_first_t, pd_first_m), pd_second = measure_cases(df.to_pandas(), reference_mean_prices.to_pandas())
+
+print(f"Modin:\n\tmean iteration time: {md_first_m}s\n\testimated case1_for_loop time: {md_first_t}s\n\tcase2_groupby_mean time: {md_second}s")
+print(f"Pandas:\n\tmean iteration time: {pd_first_m}s\n\testimated case1_for_loop time: {pd_first_t}s\n\tcase2_groupby_mean time: {pd_second}s")
--- sequence_of_heavy_ops.py
+++ sequence_of_heavy_ops.py
@@ -8,6 +8,8 @@ NUM_BRANCHES = 500_000
 NUM_CITIES = 500
 NCOLS = 10
 
+print("generating data...")
+
 data = {
     "Branch": np.tile(np.arange(NUM_BRANCHES), NROWS // NUM_BRANCHES),
     "City": np.tile([f"City{i}" for i in range(NUM_CITIES)], NROWS // NUM_CITIES),
@@ -33,6 +35,7 @@ from modin.pandas.io import from_pandas
 # initialize Ray
 from_pandas(pd.DataFrame({"a": [1]}))
 
+print("data generated...")
 
 def pure_pandas(df):
     filtered = df.query("Price < 100_000 & BuyCount > 0")
@@ -60,7 +63,7 @@ def modin_one_conversion(df):
 
 
 def modin_several_conversions(df):
-    filtered = from_pandas(df).query("Price < 100_000 & BuyCount > 0")
+    filtered = from_pandas(df).query("Price < 100_000 & BuyCount > 0").to_pandas()
     stats_per_branch = (
         from_pandas(filtered).groupby("Branch").apply(calc_stats, include_groups=False)
     )
@@ -68,15 +71,12 @@ def modin_several_conversions(df):
         from_pandas(filtered).groupby("City").apply(calc_stats, include_groups=False)
     )
 
-    merged = filtered.merge(stats_per_branch, on="Branch").merge(
+    merged = from_pandas(filtered).merge(stats_per_branch, on="Branch").merge(
         stats_per_city, on="City"
     )
     merged = merged.to_pandas()
 
-
-t0 = timer()
-pure_pandas(df)
-print("pure pandas:", timer() - t0)
+print("running test cases for modin...")
 
 t0 = timer()
 modin_one_conversion(df)
@@ -85,3 +85,9 @@ print("modin one conversion:", timer() - t0)
 t0 = timer()
 modin_several_conversions(df)
 print("modin several conversions:", timer() - t0)
+
+print("running test cases for pandas...")
+
+t0 = timer()
+pure_pandas(df)
+print("pure pandas:", timer() - t0)
